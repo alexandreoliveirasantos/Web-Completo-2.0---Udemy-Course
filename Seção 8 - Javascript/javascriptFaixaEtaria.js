6282f0c106af5a3d4b5906a969f8df2e1e110b31
Alexandre Oliveira <alexandre121996@hotmail.com>
2018-11-03 09:59:27 -0300

    Transferência inicial do repositório local
    Transferência dos códigos desenvolvidos localmente para Github. A partir desse momento todo o conteúdo estudado e desenvolvido será comitado diretamente para esse repositório.

---
+++ Seção 8 - Javascript/javascriptFaixaEtaria.js
@@ -0,0 +1,11 @@
+var idade = prompt("Insira a idade: ")
+
+if(idade >= 0 && idade < 15){
+	document.write("Criança")
+}else if(idade >= 15 && idade < 30){
+	document.write("Jovem")
+}else if(idade >= 30 && idade < 60){
+	document.write("Adulto")
+}else if(idade >= 60){
+	document.write("Idoso")
+}
